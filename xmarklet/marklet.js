83bf5b263713ccf2cc819974cd888b4438911d29
Malte Burkhardt <info@bitsundpixel.de>
2017-03-22 23:53:04 +0100

    marklet code

---
+++ xmarklet/marklet.js
@@ -0,0 +1,8 @@
+/**
+ * Created by malte on 22.03.17.
+ */
+(function () {
+    let jsCode = document.createElement('script');
+    jsCode.setAttribute('src', 'http://mbrk.github.io/xmarklet/main.js');
+    document.body.appendChild(jsCode);
+}());
